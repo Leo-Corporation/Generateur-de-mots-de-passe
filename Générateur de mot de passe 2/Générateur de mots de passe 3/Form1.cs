d39bb07660e1ea47fbf82d23994868b5ce6c016d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-03-03 14:21:10 +0100

    Ajout de commentaires sur le code.

--- Générateur de mot de passe 2/Générateur de mots de passe 3/Form1.cs
+++ Générateur de mot de passe 2/Générateur de mots de passe 3/Form1.cs
@@ -19,14 +19,15 @@ namespace Générateur_de_mots_de_passe_3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Guna.UI.Lib.GraphicsHelper.ShadowForm(this);
+            Guna.UI.Lib.GraphicsHelper.ShadowForm(this); // Ajout de l'ombre sur Form1 (this)
         }
 
         private void gunaAdvenceButton4_Click(object sender, EventArgs e)
         {
+            // Système de génération d'un nombre aléatoire entre 9 et 20
             Random random = new Random();
-            int number = random.Next(10, 20);
-            gunaLineTextBox1.Text = number.ToString();
+            int number = random.Next(9, 20);
+            gunaLineTextBox1.Text = number.ToString(); // Le nombre va dans la textbox qui demande le nombre de caractères
         }
     }
 }
