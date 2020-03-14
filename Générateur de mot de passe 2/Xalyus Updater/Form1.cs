7e57e090a6e2b56a079cdcc02cc287b510f8a183
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-03-14 12:10:36 +0100

    Ajout de Xalyus Updater #37

---
+++ Générateur de mot de passe 2/Xalyus Updater/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Xalyus_Updater
+{
+    public partial class Form1 : Form
+    {
+        WebClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath + "/Générateur de mots de passe 3.exe";
+            File.Delete(path);
+            client = new WebClient();
+            WebClient maj = new WebClient();
+            string link = maj.DownloadString("https://dl.dropboxusercontent.com/s/sj1je1zt21amtdw/Software%20Download.txt");
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadFileCompleted += Client_DownloadFileCompleted;
+            if (!string.IsNullOrEmpty(link))
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Uri uri = new Uri(link);
+                    client.DownloadFileAsync(uri, path);
+                });
+                thread.Start();
+            }
+        }
+
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                Process.Start(Application.StartupPath + "/Générateur de mots de passe.exe");
+                Close();
+            }));
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                gunaProgressBar1.Minimum = 0;
+                double receive = double.Parse(e.BytesReceived.ToString());
+                double total = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = receive / total * 100;
+                gunaLabel6.Text = $"{string.Format("{0:0.##}", percentage)}%";
+                gunaLabel6.Left = (this.ClientSize.Width - gunaLabel6.Width) / 2;
+                gunaProgressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            }));
+        }
+    }
+}
