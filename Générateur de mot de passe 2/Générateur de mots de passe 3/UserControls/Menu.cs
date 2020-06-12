988d43381118a4e337109edcda659c9754487886
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-06-12 12:12:47 +0200

    Le thème sombre n'est pas compatible avec le menu (#66)

--- Générateur de mot de passe 2/Générateur de mots de passe 3/UserControls/Menu.cs
+++ Générateur de mot de passe 2/Générateur de mots de passe 3/UserControls/Menu.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Guna.UI.WinForms;
+using System.Runtime.CompilerServices;
 
 namespace Générateur_de_mots_de_passe_3.UserControls
 {
@@ -22,6 +23,19 @@ namespace Générateur_de_mots_de_passe_3.UserControls
         {
             Check(sender);
             UpdateSettings();
+            foreach (GunaAdvenceButton advenceButton in Controls)
+            {
+                if (advenceButton.Checked)
+                {
+                    advenceButton.ImageSize = new Size(15, 15);
+                    advenceButton.ImageOffsetX = -4;
+                }
+                else
+                {
+                    advenceButton.ImageSize = new Size(25, 25);
+                    advenceButton.ImageOffsetX = -4;
+                }
+            }
         }
 
         private void Check(object sender)
@@ -51,6 +65,7 @@ namespace Générateur_de_mots_de_passe_3.UserControls
             {
                 Properties.Settings.Default.DefaultPreset = "Personnalisé";
             }
+            Properties.Settings.Default.Save();
         }
 
         private void Menu_Load(object sender, EventArgs e)
@@ -74,6 +89,51 @@ namespace Générateur_de_mots_de_passe_3.UserControls
                     gunaAdvenceButton2.Checked = true;
                     gunaAdvenceButton2.ImageSize = new Size(15, 15); // Changer la taille de l'image
                     break;
+            }
+        }
+
+        private int _theme;
+
+        public int ThemeSet
+        {
+            get
+            {
+                return _theme;
+            }
+            set
+            {
+                _theme = value;
+                ChangeTheme(value);
+            }
+        }
+
+        private void ChangeTheme(int themeId)
+        {
+            if (themeId == 0) // Si le thème est clair
+            {
+                BackColor = Color.White;
+                gunaAdvenceButton1.BaseColor = Color.White;
+                gunaAdvenceButton2.BaseColor = Color.White;
+                gunaAdvenceButton5.BaseColor = Color.White;
+                gunaAdvenceButton1.ForeColor = Color.Black;
+                gunaAdvenceButton2.ForeColor = Color.Black;
+                gunaAdvenceButton5.ForeColor = Color.Black;
+                gunaAdvenceButton1.Image = Properties.Resources.advanced_processor_black;
+                gunaAdvenceButton2.Image = Properties.Resources.edit_black;
+                gunaAdvenceButton5.Image = Properties.Resources.processor_black;
+            }
+            else // Si le thème est sombre
+            {
+                BackColor = Color.FromArgb(50, 50, 72);
+                gunaAdvenceButton1.BaseColor = Color.FromArgb(50, 50, 72);
+                gunaAdvenceButton2.BaseColor = Color.FromArgb(50, 50, 72); ;
+                gunaAdvenceButton5.BaseColor = Color.FromArgb(50, 50, 72); ;
+                gunaAdvenceButton1.ForeColor = Color.White;
+                gunaAdvenceButton2.ForeColor = Color.White;
+                gunaAdvenceButton5.ForeColor = Color.White;
+                gunaAdvenceButton1.Image = Properties.Resources.advanced_processor;
+                gunaAdvenceButton2.Image = Properties.Resources.edit;
+                gunaAdvenceButton5.Image = Properties.Resources.processor;
             }
         }
     }
