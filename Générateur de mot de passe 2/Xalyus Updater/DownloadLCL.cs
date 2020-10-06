6a9f959328bb6a616a24970617e673e4e33ff0f8
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-10-06 16:38:24 +0200

    Adapter Xalyus Updater (#92)

---
+++ Générateur de mot de passe 2/Xalyus Updater/DownloadLCL.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Xalyus_Updater
+{
+    public partial class DownloadLCL : Form
+    {
+        WebClient client = new WebClient();
+        public DownloadLCL()
+        {
+            InitializeComponent();
+        }
+
+        private void DownloadLCL_Load(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath + "/LeoCorpLibrary.dll";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(Application.StartupPath + "/fr-FR");
+            }
+            File.Delete(path);
+            client = new WebClient();
+            WebClient maj = new WebClient();
+            string link = maj.DownloadString("https://raw.githubusercontent.com/Leo-Corporation/LeoCorp-Docs/master/Liens/Update%20System/G%C3%A9n%C3%A9rateur%20de%20mots%20de%20passe%203/downloadLCL.txt");
+
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadFileCompleted += Client_DownloadFileCompleted;
+
+            if (!string.IsNullOrEmpty(link))
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Uri uri = new Uri(link);
+                    client.DownloadFileAsync(uri, path);
+                });
+                thread.Start();
+            }
+        }
+
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                Close();
+            }));
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                gunaProgressBar1.Minimum = 0;
+                double receive = double.Parse(e.BytesReceived.ToString());
+                double total = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = receive / total * 100;
+                gunaLabel6.Text = $"{string.Format("{0:0.##}", percentage)}%";
+                gunaLabel6.Left = (ClientSize.Width - gunaLabel6.Width) / 2;
+                gunaProgressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            }));
+        }
+    }
+}
